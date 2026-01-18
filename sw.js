8fcbf1a5a5daf8c8bc7af4675f507438ff9fd35b
zchudacek-tech <z.chudacek@seznam.cz>
2026-01-18 10:07:47 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "triathlon-pro-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
